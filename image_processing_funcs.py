41227655daedce091d67fb7b53564572fb5951e0
Ayesha Bajwa <abajwa@mit.edu>
2017-05-19 15:25:00 +0100

    tuning contour finding, improved on some images, size still an issue

--- image_processing_funcs.py
+++ image_processing_funcs.py
@@ -6,20 +6,42 @@ from skimage.color import rgb2gray
 from skimage.exposure import equalize_hist
 from skimage.filters import gaussian
 from skimage.segmentation import active_contour
+from skimage.transform import resize
 import matplotlib.patches as mpatches
 import matplotlib.path as mpath
 
 
 def main():
-	segment_tympanic_membrane('./ear_images/normalTympanic1.jpg') #cone of light throws off
+	#segment_tympanic_membrane('./ear_images/normalTympanic1.jpg') #cone of light throws off
 	#segment_tympanic_membrane('./ear_images/noisy1-1.jpg')
 	#segment_tympanic_membrane('./ear_images/noisy1-2.jpg')
-	segment_tympanic_membrane('./ear_images/normalTympanic2.jpg')
-	segment_tympanic_membrane('./ear_images/normalTympanic3.jpg')
+	#segment_tympanic_membrane('./ear_images/normalTympanic2.jpg') #handles well
+	#segment_tympanic_membrane('./ear_images/normalTympanic3.jpg') #handles well
 
-	segment_tympanic_membrane('./normal_ear_images/NORMAL1.jpg')
-	segment_tympanic_membrane('./normal_ear_images/NORMAL2.jpg') #all black
-	segment_tympanic_membrane('./normal_ear_images/NORMAL3.jpg') #too much extra space
+	#segment_tympanic_membrane('./normal_ear_images/NORMAL1.jpg') #handles well
+	#segment_tympanic_membrane('./normal_ear_images/NORMAL2.jpg') #all black
+	#segment_tympanic_membrane('./normal_ear_images/NORMAL3.jpg') #probably stuck
+	#segment_tympanic_membrane('./normal_ear_images/NORMAL3-1.jpg')
+	#segment_tympanic_membrane('./normal_ear_images/NORMAL4.jpg') #probably getting stuck on circle, no output
+	#segment_tympanic_membrane('./normal_ear_images/NORMAL5.jpg') #probably getting stuck on circle, no output
+	#segment_tympanic_membrane('./normal_ear_images/NORMAL6.jpg')
+	segment_tympanic_membrane('./normal_ear_images/NORMAL7.jpg') # bad - edge stuck?
+	#segment_tympanic_membrane('./normal_ear_images/NORMAL8.jpg')
+	segment_tympanic_membrane('./normal_ear_images/NORMAL8-1.jpg')
+	segment_tympanic_membrane('./normal_ear_images/NORMAL9-1.jpg')
+	segment_tympanic_membrane('./normal_ear_images/NORMAL9.jpg')
+	#segment_tympanic_membrane('./normal_ear_images/NORMAL10.jpg') - too big
+	segment_tympanic_membrane('./normal_ear_images/NORMAL11.jpg') #handles okay
+	segment_tympanic_membrane('./normal_ear_images/NORMAL12.jpg') #handles well
+	segment_tympanic_membrane('./normal_ear_images/NORMAL13.jpg')
+	segment_tympanic_membrane('./normal_ear_images/NORMAL14.jpg')
+	segment_tympanic_membrane('./normal_ear_images/NORMAL15.jpg')
+	#segment_tympanic_membrane('./normal_ear_images/NORMAL16.jpg') # black
+	# segment_tympanic_membrane('./normal_ear_images/NORMAL17.jpg') # black
+	segment_tympanic_membrane('./normal_ear_images/NORMAL18.jpg') # WAY off
+	segment_tympanic_membrane('./normal_ear_images/NORMAL19.jpg') # not close ehough
+	segment_tympanic_membrane('./normal_ear_images/NORMAL20.jpg')
+	#segment_tympanic_membrane('./normal_ear_images/NORMAL27.jpg')
 
 
 #TODO
@@ -27,6 +49,10 @@ def segment_tympanic_membrane(filename):
 	"""Given image input containing tympanic membrane, segments by masking
 	to show only membrane and displays/saves masked image."""
 	orig = read_image(filename)
+	orig.shape
+	#orig = scale_image(orig)
+	#orig.shape
+	#save_image(orig, 'test_scale.jpg')
 
 	#make grayscale
 	img = rgb2gray(orig)
@@ -35,10 +61,12 @@ def segment_tympanic_membrane(filename):
 	img = equalize_hist(img)
 
 	#initialize search circle in center
-	s = np.linspace(0, 2*np.pi, 400)
+	height = img.shape[0]
+	width = img.shape[1]
+	s = np.linspace(0, 2*np.pi, height)
 	#start centered and use search boundary
-	x = img.shape[1]/2 + 220*np.cos(s)
-	y = img.shape[0]/2 + 220*np.sin(s)
+	x = width/2 + width/2*np.cos(s)
+	y = height/2 + height/2*np.sin(s)
 	init = np.array([x, y]).T
 
 
@@ -60,7 +88,7 @@ def segment_tympanic_membrane(filename):
 
 	mask_outside_polygon(coords)
 	
-	ax.set_xticks([]), ax.set_yticks([])
+	#ax.set_xticks([]), ax.set_yticks([])
 	ax.axis([0, orig.shape[1], orig.shape[0], 0])
 
 	ax.imshow(orig)
@@ -121,7 +149,7 @@ def rgb_array_separation(inputArr):
 	return redArr, greenArr, blueArr;
 
 
-def rgb_aray_addition(redArr, greenArr, blueArr):
+def rgb_array_addition(redArr, greenArr, blueArr):
 	"""Given the RGB color channels of image as three numpy arrays, returns numpy array representing RGB file"""
 
 	# add channels and normalize 
@@ -132,6 +160,40 @@ def rgb_aray_addition(redArr, greenArr, blueArr):
 	sumArr = np.add(redArr, np.add(greenArr, blueArr))
 
 	return sumArr;
+
+
+def show_images(images,titles=None):
+    """Display a list of images"""
+    n_ims = len(images)
+    if titles is None: titles = ['(%d)' % i for i in range(1,n_ims + 1)]
+    fig = plt.figure()
+    n = 1
+    for image,title in zip(images,titles):
+        a = fig.add_subplot(1,n_ims,n)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+        n += 1
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_ims)
+    plt.show()
+
+
+def scale_image(numpyArray):
+	"""Given an image, scale to 500 in smaller dimension"""
+	width = numpyArray.shape[0]
+	height = numpyArray.shape[1]
+	if width < height:
+		if width > 500:
+			factor = width/500
+			resize(numpyArray, (factor*width, factor*height))
+			#rescale(numpyArray, width/500., preserve_range=True)
+	else:
+		if height > 500:
+			factor = height/500
+			resize(numpyArray, (factor*width, factor*height))
+			#rescale(numpyArray, height/500., preserve_range=True)
+	return;
 
 
 def read_image(inputFile):
