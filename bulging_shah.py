f698509e894170a761577b85d2f5aeddf8add8da
madhi1 <madhielango@outlook.com>
2017-05-22 13:08:12 +0100

    Working implementation of Shah approach with some need for manual adjustment

--- bulging_shah.py
+++ bulging_shah.py
@@ -1,10 +1,15 @@
 import numpy as np
+import scipy as sp
+from scipy import signal
 import cv2
 import matplotlib.pylab as plt
 import math
+from math import cos,sin,tan
+from numpy import sqrt
 import cmath
 from matplotlib import cm
 from mpl_toolkits.mplot3d.axes3d import Axes3D
+
 
 eps = np.finfo(float).eps
 i=cmath.sqrt(-1)
@@ -37,7 +42,8 @@ def tilt_calc():
 	
 	
 	
-img=cv2.imread('sphere.png',0)
+img=cv2.imread('./ear_AOM/AOM18.jpg',0)
+#img = cv2.resize(img, (50,50))
 img = img.astype(np.float32, copy=False)
 img=img/np.amax(img)
 height=np.shape(img)[1]
@@ -54,58 +60,39 @@ tilt=tilt_calc()
 
 ######################copied from guide###################
 
-#surface normals
 p = np.zeros((width,height))
 q = np.zeros((width,height))
-
-# the surface
 Z = np.zeros((width,height))
 
-# surface derivatives in x and y directions
 Z_x = np.zeros((width,height))
 Z_y = np.zeros((width,height))
 
-# maximum number of iterations
-maxIter = 10
+maxIter = 6
 
-# the normalized illumination direction
-ix = math.cos(tilt) * math.tan(slant)
-iy = math.sin(tilt) * math.tan(slant)
+ix = cos(tilt) * tan(slant)
+iy = sin(tilt) * tan(slant)
 
 for k in range(maxIter):
 	print(k)
-	# using the illumination direction and the currently estimate
-	# surface normals, compute the corresponding reflectance map.    
-	R =(math.cos(slant) + p* math.cos(tilt)*math.sin(slant)+ q*math.sin(tilt)*math.sin(slant))/np.sqrt(1 + p**2 + q**2)
-	# at each iteration, make sure that the reflectance map is positive at
-	# each pixel, set negative values to zero.
-	
-	for x in np.nditer(R, op_flags=['readwrite']):
-		x[...]=max(0,x)
-	# compute our function f which is the deviation of the computed
-	# reflectance map from the original image ...
+	R =(cos(slant) + p * cos(tilt)*sin(slant)+ q *sin(tilt)*sin(slant))/sqrt(1 + p**2 + q**2)
+	R = np.maximum(R,0)
 	f = img - R
-	print(f)
-	# compute the derivative of f with respect to our surface Z ... refer to (62)
-	df_dZ =(p+q)*(ix*p + iy*q + 1)/(np.sqrt((1 + p**2+ q**2)**3)*np.sqrt(1 + ix**2 + iy**2))-(ix+iy)/(np.sqrt(1 + p**2 + q**2)*np.sqrt(1 + ix**2 + iy**2))
-	# update our surface ... refer to (61)
-	Z = Z - f/(df_dZ + eps)	
-	# compute the surface derivatives with respect to x and y
+	df_dZ =(p+q)*(ix*p + iy*q + 1)/(sqrt((1 + p**2 + q**2)**3)*sqrt(1 + ix**2 + iy**2))-(ix+iy)/(sqrt(1 + p**2 + q**2)*sqrt(1 + ix**2 + iy**2))
+	Z = Z - f/(df_dZ + eps)
 	Z_x[2:width,:] = Z[1:width-1,:]
 	Z_y[:,2:height] = Z[:,1:height-1]
-	# using the updated surface, compute new surface normals, refer to (58) and (59)
 	p = Z - Z_x
 	q = Z - Z_y
 
 #####################copied from guide########################
 
-#plt.imshow(z,interpolation='nearest')
-#plt.show()
-
+Z=np.minimum(Z,np.mean(Z)+10*np.std(Z))
+Z=np.maximum(Z,np.mean(Z)-10*np.std(Z))
 
 x = range(width)
 y = range(height)
 X, Y = np.meshgrid(x, y)
+Z=sp.signal.medfilt2d(Z,15)
 hf1 = plt.figure(1)
 ax = hf1.add_subplot(111, projection='3d')
 ax.plot_surface(X,Y, np.transpose(Z),cmap=cm.coolwarm)
