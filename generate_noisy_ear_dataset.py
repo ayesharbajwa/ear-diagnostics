c468844d84105649d3236c87071bff4e75281246
Ayesha Bajwa <abajwa@mit.edu>
2017-05-25 13:37:27 +0100

    initial noisy image generation script and folders

---
+++ generate_noisy_ear_dataset.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+from skimage.util import random_noise
+from skimage.transform import rotate
+from image_processing_funcs import read_image
+from image_processing_funcs import save_image
+import random
+
+#read in original image from which to create noisy images
+
+for num in range(71):
+
+	path = './ear_normal_square/NORMAL' + str(num) + 'jpg'
+	orig = read_image(path)
+
+	for i in range(5):
+		
+		#modify image
+		new = random_noise(orig, mode='gaussian', clip=False)
+		rot_angle = random.choice([0, 90, 180, 270])
+		new = rotate(new, rot_angle)
+
+		"""
+		#for visualizing image
+		plt.gray()
+		fig = plt.figure(figsize=(7, 7))
+		ax = fig.add_subplot(111)
+		ax.imshow(new)
+		ax.set_xticks([]), ax.set_yticks([])
+		ax.axis([0, new.shape[1], new.shape[0], 0])
+		plt.show()
+		"""
+
+		#save noisy image
+		#save_image(new, './noisy_ear_images/noisy2' + str(i) + '.jpg')
+		#save_image(new, './noisy_ear_images/noisy3' + str(i) + '.jpg')
+		#save_image(new, './noisy_ear_images/noisy4' + str(i) + '.jpg')
+		save_image(new, './noisy_ear_normal_square/noisy' + str(num) + '-' + str(i) + '.jpg')
