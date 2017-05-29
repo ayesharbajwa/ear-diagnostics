1ea9318e32088727cba8c1e99e6282d4bf62ef89
Ayesha Bajwa <abajwa@mit.edu>
2017-05-29 21:28:13 +0100

    added GUI buttons and wrapping print functions

--- gui.py
+++ gui.py
@@ -16,36 +16,62 @@ class Panel:
 
 		x = tk.Label(master, text="Welcome to our GUI.", font=("Helvetica", 20))
 		x.pack()
+		self.quit = tk.Button(master, text="QUIT", highlightbackground='#3E4149', command=master.quit)
+		#self.button.pack(side=tk.LEFT)
+		self.quit.pack(pady=8, padx=15)
+
+
 
 		frame = tk.Frame(master)
-		frame.pack()
+		frame.pack(side=tk.LEFT)
 
-		self.hearing_test = tk.Button(frame, text="Hearing Test", highlightbackground='#0f0684', command=lambda: hearing_test())
-		#self.hearing_test.pack(side=tk.LEFT)
-		self.hearing_test.pack(pady=8)
+		a = tk.Label(frame, text="Basic Functions", font=("Helvetica", 18))
+		a.pack()
 
-		self.temp = tk.Button(frame, text="Ear Temperature", highlightbackground='#174854', command=lambda: self.enter_temperature())
-		#self.hi_there.pack(side=tk.LEFT)
-		self.temp.pack(pady=8)
+		self.hearing_test = tk.Button(frame, text="Hearing Test", highlightbackground='#2981ce', command=lambda: hearing_test())
+		self.hearing_test.pack(pady=8, padx=15)
 
-		self.camera = tk.Button(frame, text="Image Capture", highlightbackground='#2981ce', command=lambda: self.print_capture())
-		#self.hi_there.pack(side=tk.LEFT)
-		self.camera.pack(pady=8)
+		self.temp = tk.Button(frame, text="Ear Temperature", highlightbackground='#2981ce', command=lambda: self.enter_temperature())
+		self.temp.pack(pady=8, padx=15)
 
-		self.quit = tk.Button(frame, text="QUIT", highlightbackground='#3E4149', command=frame.quit)
-		#self.button.pack(side=tk.LEFT)
-		self.quit.pack(pady=8)
+		self.feed = tk.Button(frame, text="Get Live Feed", highlightbackground='#2981ce', command=lambda: self.print_feed())
+		self.feed.pack(pady=8, padx=15)
 
-		y = tk.Label(master, text="GM1 2017", font=("Helvetica", 15))
-		y.pack()
+		self.capture = tk.Button(frame, text="Image Capture", highlightbackground='#2981ce', command=lambda: self.print_capture())
+		self.capture.pack(pady=8, padx=15)
+
+
+
+		frame2 = tk.Frame(master)
+		frame2.pack(side=tk.RIGHT)
+
+		b = tk.Label(frame2, text="Plotting", font=("Helvetica", 18))
+		b.pack()
+
+		self.read = tk.Button(frame2, text="Read Data File", highlightbackground='#0f0684', command=lambda: self.print_read())
+		self.read.pack(pady=8, padx=15)
+
+		self.plot = tk.Button(frame2, text="Display Plot", highlightbackground='#0f0684', command=lambda: self.print_plot())
+		self.plot.pack(pady=8, padx=15)
+
+
+		frame3 = tk.Frame(master)
+		frame3.pack(side=tk.RIGHT)
+
+		c = tk.Label(frame3, text="Image Processing", font=("Helvetica", 18))
+		c.pack()
+
+		self.feed = tk.Button(frame3, text="Enter File Path", highlightbackground='#174854', command=lambda: self.enter_path())
+		self.feed.pack(pady=8, padx=15)
+
+		self.capture = tk.Button(frame3, text="Calculate Redness", highlightbackground='#174854', command=lambda: self.print_redness())
+		self.capture.pack(pady=8, padx=15)
+
+
 
 	# Run -- never returns
 	def run(self):
 		self.tk.mainloop() # Hang around...
-
-	def print_capture(self):
-		print "Image captured."
-		#return
 
 	def print_hearing(self):
 		print "Adminstering hearing test."
@@ -55,6 +81,29 @@ class Panel:
 		temp = float(input("Enter the patient ear temperature in C:"))
 		return temp
 
+	def print_feed(self):
+		print "Rolling live feed."
+		#return
+
+	def print_capture(self):
+		print "Image captured."
+		#return
+
+	def print_read(self):
+		print "Reading data file."
+		#return
+
+	def print_plot(self):
+		print "Creating plot."
+		#return
+
+	def enter_path(self):
+		path = input("Enter the file path:")
+		return path
+
+	def print_redness(self):
+		print "Calculating redness."
+		#return		
 
 def main():
 	panel = Panel()
