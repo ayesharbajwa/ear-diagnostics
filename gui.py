760a6f82a6931c4fbad421462d1e8a3c18fda9c9
Ayesha Bajwa <abajwa@mit.edu>
2017-05-30 11:05:40 +0100

    more buttons

--- gui.py
+++ gui.py
@@ -16,7 +16,7 @@ class Panel:
 
 		x = tk.Label(master, text="Welcome to our GUI.", font=("Helvetica", 20))
 		x.pack()
-		self.quit = tk.Button(master, text="QUIT", highlightbackground='#3E4149', command=master.quit)
+		self.quit = tk.Button(master, text="QUIT", font=("Helvetica", 15), highlightbackground='#3E4149', command=master.quit)
 		#self.button.pack(side=tk.LEFT)
 		self.quit.pack(pady=8, padx=15)
 
@@ -28,16 +28,16 @@ class Panel:
 		a = tk.Label(frame, text="Basic Functions", font=("Helvetica", 18))
 		a.pack()
 
-		self.hearing_test = tk.Button(frame, text="Hearing Test", highlightbackground='#2981ce', command=lambda: hearing_test())
+		self.hearing_test = tk.Button(frame, text="Hearing Test", font=("Helvetica", 15), highlightbackground='#2981ce', command=lambda: self.print_hearing())
 		self.hearing_test.pack(pady=8, padx=15)
 
-		self.temp = tk.Button(frame, text="Ear Temperature", highlightbackground='#2981ce', command=lambda: self.enter_temperature())
+		self.temp = tk.Button(frame, text="Ear Temperature", font=("Helvetica", 15), highlightbackground='#2981ce', command=lambda: self.enter_temperature())
 		self.temp.pack(pady=8, padx=15)
 
-		self.feed = tk.Button(frame, text="Get Live Feed", highlightbackground='#2981ce', command=lambda: self.print_feed())
+		self.feed = tk.Button(frame, text="Display Live Feed", font=("Helvetica", 15), highlightbackground='#2981ce', command=lambda: self.print_feed())
 		self.feed.pack(pady=8, padx=15)
 
-		self.capture = tk.Button(frame, text="Image Capture", highlightbackground='#2981ce', command=lambda: self.print_capture())
+		self.capture = tk.Button(frame, text="Capture Image", font=("Helvetica", 15), highlightbackground='#2981ce', command=lambda: self.print_capture())
 		self.capture.pack(pady=8, padx=15)
 
 
@@ -48,11 +48,13 @@ class Panel:
 		b = tk.Label(frame2, text="Plotting", font=("Helvetica", 18))
 		b.pack()
 
-		self.read = tk.Button(frame2, text="Read Data File", highlightbackground='#0f0684', command=lambda: self.print_read())
+		self.read = tk.Button(frame2, text="Read Data File", font=("Helvetica", 15), highlightbackground='#0f0684', command=lambda: self.print_read())
 		self.read.pack(pady=8, padx=15)
 
-		self.plot = tk.Button(frame2, text="Display Plot", highlightbackground='#0f0684', command=lambda: self.print_plot())
+		self.plot = tk.Button(frame2, text="Display Plot", font=("Helvetica", 15), highlightbackground='#0f0684', command=lambda: self.print_plot())
 		self.plot.pack(pady=8, padx=15)
+
+
 
 
 		frame3 = tk.Frame(master)
@@ -61,11 +63,17 @@ class Panel:
 		c = tk.Label(frame3, text="Image Processing", font=("Helvetica", 18))
 		c.pack()
 
-		self.feed = tk.Button(frame3, text="Enter File Path", highlightbackground='#174854', command=lambda: self.enter_path())
-		self.feed.pack(pady=8, padx=15)
+		self.path = tk.Button(frame3, text="Enter Image File Path", font=("Helvetica", 15), highlightbackground='#174854', command=lambda: self.enter_path())
+		self.path.pack(pady=8, padx=15)
 
-		self.capture = tk.Button(frame3, text="Calculate Redness", highlightbackground='#174854', command=lambda: self.print_redness())
-		self.capture.pack(pady=8, padx=15)
+		self.red = tk.Button(frame3, text="Analyse Redness", font=("Helvetica", 15), highlightbackground='#174854', command=lambda: self.print_redness())
+		self.red.pack(pady=8, padx=15)
+
+		self.bulge = tk.Button(frame3, text="Analyse Bulging", font=("Helvetica", 15), highlightbackground='#174854', command=lambda: self.print_bulge())
+		self.bulge.pack(pady=8, padx=15)
+
+		self.seg = tk.Button(frame3, text="Segment", font=("Helvetica", 15), highlightbackground='#174854', command=lambda: self.print_bulge())
+		self.seg.pack(pady=8, padx=15)
 
 
 
@@ -75,6 +83,7 @@ class Panel:
 
 	def print_hearing(self):
 		print "Adminstering hearing test."
+		hearing_test()
 		#return
 
 	def enter_temperature(self):
@@ -103,7 +112,11 @@ class Panel:
 
 	def print_redness(self):
 		print "Calculating redness."
-		#return		
+		#return
+
+	def print_bulge(self):
+		print "Calculating bulging."
+		#return
 
 def main():
 	panel = Panel()
